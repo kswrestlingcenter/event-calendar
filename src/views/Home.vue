828d89e0cc27aa34b023bc1cab8c18b782c2d3ca
Aldon <aisenberg@freightview.com>
2021-07-13 15:58:58 -0500

    include router

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <EventCard/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    EventCard
+  }
+}
+</script>
